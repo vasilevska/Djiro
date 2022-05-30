58061797fe4d76ed56754b41fd84f8139c1cb58f
Stefan Brankovic <79327006+stefcon@users.noreply.github.com>
2022-05-31 00:12:34 +0200

    Unfinished car card

--- Implementacija/djiro_vue/src/views/HomeView.vue
+++ Implementacija/djiro_vue/src/views/HomeView.vue
@@ -1,10 +1,15 @@
 <template>
-  <div class="home">This is home</div>
+  <div class="home">
+    This is home
+  <car-card :carInfo="null"></car-card>
+  </div>
 </template>
 
 <script>
+import CarCard from '../components/CarCard.vue';
+
 export default {
   name: "HomeView",
-  components: {},
+  components: { CarCard },
 };
 </script>
