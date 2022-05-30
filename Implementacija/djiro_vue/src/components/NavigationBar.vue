1bb77179a7540aff5803c5c18bbf46251cd51acd
Stefan Brankovic <79327006+stefcon@users.noreply.github.com>
2022-05-30 16:22:52 +0200

    Commit for requirements.txt

---
+++ Implementacija/djiro_vue/src/components/NavigationBar.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+  <b-navbar toggleable="lg" type="light" variant="light">
+    <b-navbar-brand href="#">Điro</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+         <b-navbar-nav>
+          <b-nav-item href="#">Postani Điler</b-nav-item>
+      </b-navbar-nav>
+        <b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+          <template #button-content>
+            <em>Korisnik</em>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
+</template>
+
+
+<script>
+
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
--- Implementacija/djiro_vue/src/views/HomeView.vue
+++ Implementacija/djiro_vue/src/views/HomeView.vue
@@ -1,10 +1,30 @@
 <template>
-  <div class="home">Pocetna</div>
+    <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+    </div>
 </template>
+
+<style>
+    #nav{
+        padding: 30px;
+        background-color: lightgray;
+    }
+
+    #nav a{
+        font-weight: bold;
+        color: #2c3e50
+    }
+
+    #nav a.router-link-exact-active{
+        color: #42b983
+    }
+</style>
+
 
 <script>
 export default {
-  name: "HomeView",
+  name: "NavigationBar",
   components: {},
 };
 </script>
