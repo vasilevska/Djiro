b5609b6f0b4d89a0f15d9d6a5a0b4a238a635608
Stefan Brankovic <79327006+stefcon@users.noreply.github.com>
2022-05-30 21:46:19 +0200

    Registration and login skelet

--- Implementacija/djiro_vue/src/App.vue
+++ Implementacija/djiro_vue/src/App.vue
@@ -1,14 +1,13 @@
 <template>
   <div>
     <div class="header">
-      <nav>
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link>
-      </nav>
+      <!-- <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> -->
+      <navigation-bar></navigation-bar>
     </div>
 
     <div class="container">
-      <router-view />
+      <router-view/>
     </div>
 
     <div class="footer"></div>
@@ -18,3 +17,14 @@
 <style>
 @import "../node_modules/bootstrap";
 </style>
+
+<script>
+import NavigationBar from "./components/NavigationBar.vue";
+
+export default {
+  name: "App",
+  components: {
+    NavigationBar,
+  },
+};
+</script>
