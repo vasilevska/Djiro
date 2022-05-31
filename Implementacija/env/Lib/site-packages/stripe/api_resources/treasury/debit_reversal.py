9825b10a26df64df7295ba94aeaabc14954ae3a3
Lazar Erić <lazareric2000@gmail.com>
2022-05-31 16:38:18 +0200

    Revert "env settings"
    This reverts commit 288c5e516a866e09e1b048114120334293808a4a.

--- Implementacija/env/Lib/site-packages/stripe/api_resources/treasury/debit_reversal.py
+++
@@ -1,9 +0,0 @@
-# File generated from our OpenAPI spec
-from __future__ import absolute_import, division, print_function
-
-from stripe.api_resources.abstract import CreateableAPIResource
-from stripe.api_resources.abstract import ListableAPIResource
-
-
-class DebitReversal(CreateableAPIResource, ListableAPIResource):
-    OBJECT_NAME = "treasury.debit_reversal"
