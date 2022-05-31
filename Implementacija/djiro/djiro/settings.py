56d735842a1ccf1b524cb65a2dc95db1755a1ef3
Stefan Brankovic <79327006+stefcon@users.noreply.github.com>
2022-05-31 03:37:00 +0200

    reversed mysql ports to default

--- Implementacija/djiro/djiro/settings.py
+++ Implementacija/djiro/djiro/settings.py
@@ -95,9 +95,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'djiro',
         'USER': 'root',
-        'PASSWORD': '123', #TODO: root
+        'PASSWORD': 'root', #TODO: root
         'HOST': '127.0.0.1',
-        'PORT': '3308' #TODO: 3306
+        'PORT': '3306' #TODO: 3306
     }
 }
 
