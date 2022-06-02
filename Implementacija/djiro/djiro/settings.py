73732cdc9112e9b264a33ea7da8f7692de11e1a1
Stefan Brankovic <79327006+stefcon@users.noreply.github.com>
2022-06-02 19:34:49 +0200

    jwt expiration settings

--- Implementacija/djiro/djiro/settings.py
+++ Implementacija/djiro/djiro/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import datetime
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -190,3 +191,15 @@ ACCOUNT_AUTHENTICATION_METHOD = 'email'
 STATIC_URL = "/static/"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+JWT_AUTH = {
+    # how long the original token is valid for
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(days=2),
+
+    # allow refreshing of tokens
+    'JWT_ALLOW_REFRESH': True,
+
+    # this is the maximum time AFTER the token was issued that
+    # it can be refreshed.  exprired tokens can't be refreshed.
+    'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=7),
+}
