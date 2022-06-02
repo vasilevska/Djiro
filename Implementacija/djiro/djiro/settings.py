fa9116dacaf1479b484b34b7565ebba967efddc7
Aleksa Racic <racicaleksa7@gmail.com>
2022-06-02 15:32:16 +0200

    Vozacka validation implemented

--- Implementacija/djiro/djiro/settings.py
+++ Implementacija/djiro/djiro/settings.py
@@ -72,7 +72,7 @@ ROOT_URLCONF = 'djiro.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
