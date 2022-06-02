0817a11ad560d2eaaca2246f3ad12f61f33c9570
Nevena Vasilevska <vasilevska.nevena@gmail.com>
2022-06-02 10:25:50 +0200

    vue settings

--- Implementacija/djiro_vue/src/main.js
+++ Implementacija/djiro_vue/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
+// import axios
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(router).mount("#app");
--- Implementacija/djiro_vue/src/views/AboutView.vue
+++ Implementacija/djiro_vue/src/views/AboutView.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Reservations />
   </div>
 </template>
+
+<script>
+  import Reservations from "../components/ReservationsDriver.vue";
+  export default{
+    name: "AboutView",
+    components: {
+      Reservations,
+    }
+  }
+</script>
+
