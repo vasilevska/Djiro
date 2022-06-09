880ddd814c89731c69065121e75c3fe8280a5c44
Stefan Brankovic <79327006+stefcon@users.noreply.github.com>
2022-06-09 03:55:53 +0200

    remove parse from store

--- Implementacija/djiro_vue/src/store/index.js
+++ Implementacija/djiro_vue/src/store/index.js
@@ -84,7 +84,7 @@ export default createStore({
         access: localStorage.getItem("access"),
         refresh: localStorage.getItem("refresh"),
         id: localStorage.getItem("id"),
-        user: JSON.parse(localStorage.getItem("user")),
+        user: localStorage.getItem("user"),
       });
     },
   },
